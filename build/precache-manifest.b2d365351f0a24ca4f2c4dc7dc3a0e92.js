6aa0fce77367bb63264ae6873bae3a489bc4c309
Rishbh Arora <vsryarora6879@gmail.com>
2023-08-28 20:10:39 +0530

    initial commit

---
+++ build/precache-manifest.b2d365351f0a24ca4f2c4dc7dc3a0e92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c85a6fd79b3cd68758f5ed21b4782e5d",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "cd05a20ceebe149ae479",
+    "url": "/calculator/static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "cd05a20ceebe149ae479",
+    "url": "/calculator/static/js/main.60161fbb.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
